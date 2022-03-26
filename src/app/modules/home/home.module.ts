6fd9c277ccc04db6ba51c0108151863c014dc0ee
Milagros LC <evidal0710@ich.edu.pe>
2022-03-26 15:25:21 -0500

    Add video

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,7 +5,7 @@ import { HomeRoutingModule } from './home-routing.module';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-
+import { NgParticlesModule } from "ng-particles";
 
 @NgModule({
   declarations: [
@@ -14,6 +14,7 @@ import { RouterModule } from '@angular/router';
   imports: [
   CommonModule,
     HomeRoutingModule,
+    NgParticlesModule
    /*  FormsModule,
     RouterModule */
   ],
--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tsParticles } from "tsparticles";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,6 +10,8 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
 
 }
